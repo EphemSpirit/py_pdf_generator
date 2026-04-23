ed8249e290525c682c95b7a3b1291a9f60632f23
Drew Hund <languageinvestigator@gmail.com>
2026-04-23 13:31:29 -0400

    footers added

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from fpdf import FPDF
 import pandas as pd
 #
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
-#
-# pdf.add_page()
-#
-# pdf.set_font(family="Times", style="B", size=12)
-# pdf.cell(w=10, h=12, txt="Hello There!", align="L", ln=1)
-#
-# pdf.set_font(family="Times", size=10)
-# pdf.cell(w=0, h=12, txt="Hi There!", align="L", ln=1)
-#
-# pdf.output("output.pdf")
 
 for idx, row in df.iterrows():
     pdf.add_page()
 
+    # Set Header
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=10, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.cell(w=10, h=12, text=row["Topic"], align="L")
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for _ in range(row["Pages"] - 1):
+        pdf.add_page()
+        # Set the footer
+        pdf.ln(277)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, text=row["Topic"], align="R")
 
 pdf.output("output.pdf")
